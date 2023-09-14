de60d0f4abc61541a051e79715568c5922ee70e0
Rahad31 <rahad3100@gmail.com>
2023-09-15 02:27:12 +0600

    Fetch Course Data & Display

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
-
+import Course from "../course/course";
 const Courses = () => {
   const [courses, setCourses] = useState([]);
   useEffect(() => {
     fetch("course.json")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setCourses(data));
   }, []);
-  return <div></div>;
+  return (
+    <div className="w-2/3 flex flex-row flex-wrap justify-center items-center gap-6">
+      {courses.map((course) => (
+        <Course key={course.id} course={course}></Course>
+      ))}
+    </div>
+  );
 };
 
 export default Courses;
